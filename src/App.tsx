953ae180d262980cdaeee957d0dd9d03ac57eb38
David Kočnar <davidkocnar@seznam.cz>
2020-12-01 23:55:28 +0100

    Fixed App.tsx, added Firestore file with Firestore initialization and test read of data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
+import * as DB from './firebase/Firestore'
 import './App.css';
 
+const test = () => {
+    DB.getAllOffers()
+        .then(list => {console.log("Offers: " + list)})
+}
+
+test()
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
