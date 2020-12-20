c6e589e5dc847474ff2169015aa1401f6da76a44
David Kočnar <davidkocnar@seznam.cz>
2020-12-20 20:38:25 +0100

    Display offers created by user in profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,15 @@
 import React, {FC, useEffect, useState} from "react";
 import {useLoggedInUser} from "../firebase/auth";
-import {UserData, usersCollection} from "../firebase/firestore";
+import {Offer, offersCollection, UserData, usersCollection} from "../firebase/firestore";
 import {Box, CircularProgress, Typography} from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
+import OfferCard from "../components/OfferCard";
 
 const Profile: FC = () => {
 
   const user = useLoggedInUser();
   const [userData, setUserData] = useState<UserData>();
+  const [offers, setOffers] = useState<Offer[]>([]);
 
   useEffect(() => {
     (async () => {
@@ -18,6 +20,14 @@ const Profile: FC = () => {
         } catch (error) {
           console.error(error);
         }
+
+        offersCollection
+          .where("userRef", "==", { uid: user.uid, email: user.email })
+          .get()
+          .then((snapshot) => {
+            console.log("OFFERS", snapshot.docs.map(doc => doc.data()));
+            setOffers(snapshot.docs.map(doc => doc.data()));
+          })
       }
     })()
   }, [user])
@@ -29,14 +39,36 @@ const Profile: FC = () => {
           <Box mt="5rem"><CircularProgress/></Box>
         ) : (
           <Box mt="3rem">
-            <Typography variant={"h3"}>{userData?.name} {userData?.surname? userData?.surname : ""}</Typography>
-            <Typography variant={"body1"} style={{marginTop: "0.5rem"}}>{userData?.location}</Typography>
-            <Typography variant={"body1"} style={{marginTop: "2rem"}}>
+            <Typography variant={"h3"}>{userData?.name} {userData?.surname ? userData?.surname : ""}</Typography>
+            <Typography variant={"body1"} style={{ marginTop: "0.5rem" }}>{userData?.location}</Typography>
+            <Typography variant={"body1"} style={{ marginTop: "2rem" }}>
               E-mail: {user?.email}
             </Typography>
-            <Typography variant={"body1"} style={{marginTop: "0.5rem"}}>
-              Telefon: {userData?.phone? userData.phone : "nezadán"}
+            <Typography variant={"body1"} style={{ marginTop: "0.5rem" }}>
+              Telefon: {userData?.phone ? userData.phone : "nezadán"}
             </Typography>
+
+            <Box mt={6}>
+              <Typography variant={"h5"} style={{ marginBottom: "1rem" }}>
+                Vaše nabídky
+              </Typography>
+
+              {offers.length > 0 ?
+                <Grid item container xs={12} lg={12} spacing={2}>
+                  {offers.map((offer) => (
+                    <OfferCard
+                      price={offer.price}
+                      imgPath={offer.imgPath}
+                      title={offer.title}
+                    />
+                  ))}
+                </Grid>
+                :
+                <Typography variant={"body2"} style={{ color: "#848484" }}>
+                  Nemáte zatím žádné vytvořené nabídky
+                </Typography>
+              }
+            </Box>
           </Box>
         )}
       </Grid>
