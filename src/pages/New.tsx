e951afba668c86ff6c7023fd81623129f62a1090
PavelV <pavelvasina@gmail.com>
2020-12-21 17:02:05 +0100

    Change add offer form to react hook form

--- src/pages/New.tsx
+++ src/pages/New.tsx
@@ -4,11 +4,26 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/styles';
 import { Button, Typography } from '@material-ui/core';
 import { useLoggedInUser } from '../firebase/auth';
-import { offersCollection, categoriesCollection, timestampNow, fileStorage, Category } from '../firebase/firestore';
+import {
+  offersCollection,
+  categoriesCollection,
+  timestampNow,
+  fileStorage,
+  Category
+} from '../firebase/firestore';
 import { Redirect } from 'react-router';
 import { DropzoneArea } from 'material-ui-dropzone';
 import MenuItem from '@material-ui/core/MenuItem';
+import { useForm, Controller } from "react-hook-form";
 
+type OfferFormData = {
+  title: string;
+  description: string;
+  price: string,
+  category: string,
+  email: string;
+  phone: string
+}
 
 const useStyles = makeStyles({
   offerInput: {
@@ -17,30 +32,59 @@ const useStyles = makeStyles({
   },
   newOfferContainer: {
     padding: 20
+  },
+  offerForm: {
+    width: 100
   }
 });
 
 const New: FC = () => {
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [price, setPrice] = useState<string>("");
+
   const [images, setImages] = useState<string[]>([]);
-  const [phone, setPhone] = useState<string>("");
   const [redirect, setRedirect] = useState<boolean>(false);
   const [availableCategories, setAvailableCategories] = useState<Category[]>([])
-  const [selectedCategory, setCategory] = useState<string>("")
+  const [error, setError] = useState<string>();
+  const { control, handleSubmit, errors: fieldErrors } = useForm<OfferFormData>();
 
   const user = useLoggedInUser();
+
+  const onSubmit = async (data: OfferFormData) => {
+    if(user){
+      const categoryDocs = await categoriesCollection
+        .where("name", "==", data.category)
+        .get();
+      const categoryRef = categoryDocs.docs.map(doc => doc.ref)[0];
+
+      try {
+        await offersCollection.add({
+          userRef: {
+            uid: user.uid,
+            email: user.email
+          },
+          imgPaths: images,
+          title: data.title,
+          description: data.description,
+          price: data.price,
+          created: timestampNow(),
+          phone: data.phone,
+          categoryRef: categoryRef
+        });
+        setRedirect(true);
+      }
+      catch (error) {
+        setError(error);
+      }
+    }
+  }
 
   useEffect(() => {
     (async () => {
       try {
         const data = await categoriesCollection.get();
-        console.log(data.docs.map(doc => doc.data()))
         setAvailableCategories(data.docs.map(doc => doc.data()));
       }
       catch (error) {
-        console.error(error);
+        setError(error);
       }
     })()
   },[])
@@ -56,41 +100,10 @@ const New: FC = () => {
           imagesURLs.push(imgUrl);
         }
         catch (error) {
-          console.error(error);
+          setError(error);
         }
       });
       setImages(imagesURLs);
-    }
-  }
-
-
-  const submitOffer = async () => {
-    if(user){
-      const categoryDocs = await categoriesCollection
-        .where("name", "==", selectedCategory)
-        .get();
-
-      const categoryRef = categoryDocs.docs.map(doc => doc.ref)[0];
-
-      try{
-        await offersCollection.add({
-          userRef: {
-            uid: user.uid,
-            email: user.email
-          },
-          imgPaths: images,
-          title,
-          description,
-          price,
-          created: timestampNow(),
-          phone,
-          categoryRef: categoryRef
-        });
-        setRedirect(true);
-      }
-      catch(error){
-        console.error(error);
-      }
     }
   }
 
@@ -107,78 +120,171 @@ const New: FC = () => {
         <Typography variant="h4">Vložit inzerát</Typography>
       </Grid>
 
-      <Grid container item spacing={2} alignContent="center" justify="center">
-        <Grid item xs={12} lg={7}>
-          <DropzoneArea
-            acceptedFiles={['image/*']}
-            dropzoneText={"Přetáhněte obrázky nebo je nahrajte kliknutím"}
-            filesLimit={5}
-            maxFileSize={1000000}
-            onChange={(files) => { uploadImages(files) }} />
-        </Grid>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ width: "100%" }}>
 
-        <Grid item xs={12} lg={7}>
-          <TextField className={classes.offerInput}
-            label="Co prodáváte? (max. 60 znaků)"
-            variant="outlined"
-            fullWidth
-            onChange={e => setTitle(e.target.value)} />
-        </Grid>
+        <Grid container item spacing={2} alignContent="center" justify="center">
+          <Grid item xs={12} lg={7}>
+            <DropzoneArea
+              acceptedFiles={['image/*']}
+              dropzoneText={"Přetáhněte obrázky nebo je nahrajte kliknutím"}
+              filesLimit={5}
+              maxFileSize={1000000}
+              onChange={(files) => { uploadImages(files) }} />
+          </Grid>
 
-        <Grid item xs={12} lg={7}>
-          <TextField className={classes.offerInput}
-            label="Detaily (max. 1 000 znaků)"
-            variant="outlined"
-            fullWidth
-            multiline
-            rows={8}
-            rowsMax={8}
-            onChange={e => setDescription(e.target.value)} />
-        </Grid>
+          <Grid item xs={12} lg={7}>
+            <Controller
+              name="title"
+              as={
+                <TextField className={classes.offerInput}
+                  label="Co prodáváte? (max. 60 znaků)"
+                  variant="outlined"
+                  fullWidth
+                  helperText={fieldErrors.title ? fieldErrors.title.message : null}
+                  error={fieldErrors.title !== undefined}
+                />
+              }
+              control={control}
+              defaultValue=""
+              rules={{
+                required: 'Vyplňte název prodávaného produktu'
+              }}
+            />
+          </Grid>
 
-        <Grid item xs={12} lg={7}>
-          <TextField className={classes.offerInput}
-            label="Vyberte kategorii"
-            variant="outlined"
-            fullWidth
-            select
-            onChange={e => setCategory(e.target.value)}>
-            {availableCategories.map((category, i) => (
-              <MenuItem key={i} value={category.name}>
-                {category.name}</MenuItem>
-            ))}
-          </TextField>
-        </Grid>
+          <Grid item xs={12} lg={7}>
+            <Controller
+              name="description"
+              as={
+                <TextField className={classes.offerInput}
+                  label="Detaily (max. 1 000 znaků) (nepovinné)"
+                  variant="outlined"
+                  fullWidth
+                  multiline
+                  rows={8}
+                  rowsMax={8}
+                  helperText={fieldErrors.description ? fieldErrors.description.message : null}
+                  error={fieldErrors.description !== undefined}
+                />
+              }
+              control={control}
+              defaultValue=""
+              rules={{}}
+            />
+          </Grid>
 
-        <Grid item xs={12} lg={7}>
-          <TextField className={classes.offerInput}
-            label="Cena"
-            variant="outlined"
-            fullWidth
-            onChange={e => setPrice(e.target.value)} />
-        </Grid>
+          <Grid item xs={12} lg={7}>
+            <Controller
+              name="category"
+              as={
+                <TextField className={classes.offerInput}
+                  label="Vyberte kategorii"
+                  variant="outlined"
+                  fullWidth
+                  select
+                  helperText={fieldErrors.category ? fieldErrors.category.message : null}
+                  error={fieldErrors.category !== undefined}
+                >
+                  {availableCategories.map((category, i) => (
+                    <MenuItem key={i} value={category.name}>
+                      {category.name}
+                    </MenuItem>))}
+                </TextField>
+              }
+              control={control}
+              defaultValue=""
+              rules={{
+                required: 'Vyberte kategorii prodávaného produktu'
+              }}
+            />
+          </Grid>
 
-        <Grid item xs={12} lg={7}>
-          <TextField className={classes.offerInput}
-            label="E-mail"
-            variant="outlined"
-            fullWidth
-            value={user?.email ?? ""} />
-        </Grid>
+          <Grid item xs={12} lg={7}>
+            <Controller
+              name="price"
+              as={
+                <TextField className={classes.offerInput}
+                  label="Cena"
+                  variant="outlined"
+                  fullWidth
+                  helperText={fieldErrors.price ? fieldErrors.price.message : null}
+                  error={fieldErrors.price !== undefined}
+                />
+              }
+              control={control}
+              defaultValue=""
+              rules={{
+                required: 'Vyplňte cenu prodávaného produktu',
+                pattern: {
+                  value: /^[0-9]{1,9}$/i,
+                  message: 'Špatný formát ceny'
+                }
+              }}
+            />
+          </Grid>
 
-        <Grid item xs={12} lg={7}>
-          <TextField className={classes.offerInput}
-            label="Telefon"
-            variant="outlined"
-            fullWidth
-            onChange={e => setPhone(e.target.value)} />
-        </Grid>
+          <Grid item xs={12} lg={7}>
+            <Controller
+              name="email"
+              as={
+                <TextField className={classes.offerInput}
+                  label="E-mail"
+                  variant="outlined"
+                  value={user?.email ?? ""}
+                  fullWidth
+                  helperText={fieldErrors.email ? fieldErrors.email.message : null}
+                  error={fieldErrors.email !== undefined}
+                />
+              }
+              control={control}
+              defaultValue={user?.email ?? ""}
+              rules={{
+                required: 'Vyplňte kontaktní e-mail',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: 'Špatný formát e-mailové adresy'
+                }
+              }}
+            />
+          </Grid>
 
-        <Grid item xs={12} lg={7} className={classes.offerInput}>
-          <Button size="large" color="secondary" variant="contained"
-            onClick={submitOffer}>Vložit inzerát</Button>
+          <Grid item xs={12} lg={7}>
+            <Controller
+              name="phone"
+              as={
+                <TextField className={classes.offerInput}
+                  label="Telefon (nepovinné)"
+                  variant="outlined"
+                  fullWidth
+                  helperText={fieldErrors.phone ? fieldErrors.phone.message : null}
+                  error={fieldErrors.phone !== undefined}
+                />
+              }
+              control={control}
+              defaultValue=""
+              rules={{}}
+            />
+          </Grid>
+
+          <Grid item xs={12} lg={7}>
+            {error && (
+              <Typography variant="subtitle2" align="left" color="error" paragraph>
+                <b>{error}</b>
+              </Typography>
+            )}
+          </Grid>
+
+          <Grid item xs={12} lg={7}>
+            <Button
+              color="primary"
+              type="submit"
+              size="large"
+              variant="contained">
+              Vložit inzerát
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
     </Grid>
   )
 };
